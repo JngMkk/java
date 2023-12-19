3a23baa2010d1d02a9c2df7842426300e52d64be
JngMkk <kjm0817ss@gmail.com>
2023-12-19 18:17:33 +0900

    Java Boolean

---
+++ Types/src/Boolean/Boolean.java
@@ -0,0 +1,14 @@
+package Boolean;
+
+public class Boolean {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        boolean tf = true;
+        System.out.println(tf);
+        System.out.println("3".equals("2"));
+        if (x > y) {
+            System.out.println("true!");
+        }
+    }
+}
