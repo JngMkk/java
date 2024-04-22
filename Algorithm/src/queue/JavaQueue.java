5debb11c39ba3650369b68c1190314a845ad28ae
JngMkk <kjm0817ss@gmail.com>
2024-04-22 15:57:57 +0900

    java queue

---
+++ Algorithm/src/queue/JavaQueue.java
@@ -0,0 +1,23 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class JavaQueue {
+     public static void main(String[] args) {
+         Queue<Integer> queue = new LinkedList<>();
+
+         queue.offer(5);
+         queue.offer(2);
+         queue.offer(3);
+         queue.offer(7);
+         queue.poll();
+         queue.offer(1);
+         queue.offer(4);
+         queue.poll();
+
+         while (!queue.isEmpty()) {
+             System.out.println(queue.poll());
+         }
+     }
+}
